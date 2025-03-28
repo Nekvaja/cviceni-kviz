84bf0fc4625e7f10c8a8d83104208ac0a7ace1c1
Jana Nekvapilová <jana.nekva@gmail.com>
2025-03-28 16:02:43 +0100

    nastavení kvízové otázky a odpovědi dle zadání

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const questionElement = document.querySelector(".question");
+questionElement.innerHTML="<p>Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.</p>";
+const odpovedUzivatele = prompt("Zadej odpověď:");
+const answerText = document.querySelector(".answer__text");
+answerText.textContent = odpovedUzivatele;
+let answerElement = document.querySelector(".answer");
+
+if (odpovedUzivatele === "Bell") {
+    answerElement.classList.add("answer--correct");
+} else {
+    answerElement.classList.add("answer--wrong");
+};
+
+
